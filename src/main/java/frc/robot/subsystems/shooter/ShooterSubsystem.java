2b5e624b5277e0320d9f6b568e9c2029c84f7abf
Due Westerners <frc-programmers@duewestrobotics.org>
2026-01-22 20:51:26 -0500

    added base shooter files

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    
+
+    
+    public ShooterSubsystem() {
+        
+    }
+
+    @Override
+    public void periodic() {
+        
+    }
+}
