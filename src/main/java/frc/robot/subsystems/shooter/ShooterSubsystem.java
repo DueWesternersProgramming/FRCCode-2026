0be728d0555f14646e67b4e637a5b1f5969d4e02
Due Westerners <frc-programmers@duewestrobotics.org>
2026-03-31 20:46:59 -0400

    Hartville

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -36,7 +36,7 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public double getTimeOfFlightFromDistance(double distanceMeters){
-        return .1; //TODO: Make this equation
+        return 1; //TODO: Make this equation
     }    
 
     @Override
