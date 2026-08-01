d31923fee905f7a49d8007704c7deef92bb4d562
Due Westerners <frc-programmers@duewestrobotics.org>
2026-08-01 09:05:44 -0400

    fixed a merge error

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystemConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystemConstants.java
@@ -16,18 +16,11 @@ public class VisionSubsystemConstants {
     public static final record AprilTagCameraConfig(VisionSource source, int servoPort,
             SimCameraConfig simConfig) {
     }
-    public static final record AprilTagCameraConfig(VisionSource source, int servoPort,
-            SimCameraConfig simConfig) {
-    }
 
     public record VisionSource(String name, Transform3d robotToCamera) {
     }
-    public record VisionSource(String name, Transform3d robotToCamera) {
-    }
 
-    public static final Optional<VisionSystemSim> aprilTagSim = RobotModes.currentMode == RobotModes.simMode
-            ? Optional.of(new VisionSystemSim("AprilTagSim"))
-            : Optional.empty();
+
     public static final Optional<VisionSystemSim> aprilTagSim = RobotModes.currentMode == RobotModes.simMode
             ? Optional.of(new VisionSystemSim("AprilTagSim"))
             : Optional.empty();
